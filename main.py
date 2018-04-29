cb1dbe5aabd3f9bbe32cc18ab022402c0649b05e
supresky <nanan2468@yahoo.com.tw>
2018-04-29 19:16:07 +0800

    Now it can train

--- main.py
+++ main.py
@@ -6,21 +6,32 @@ height = 300
 picI = Image.open("images/I.png")
 picE = Image.open("images/E.png")
 picEP = Image.open("images/Eprime.png")
-picK1 = Image.open("images/k1.png")
-picK2 = Image.open("images/k2.png")
+picK1 = Image.open("images/key1.png")
+picK2 = Image.open("images/key2.png")
 
 SEN = 1
-epoch = 1
-rate = 1e-6
-w = [1e9, 1e9, 1e9]
+epoch = 25
+rate = 1e-8
+
 
 def mseGD():
     nowEpoch = 1
-    wNow = [1, 1, 1]
-    while nowEpoch==1 or nowEpoch<epoch &&:
+    w = [0, 0, 0]
+    while nowEpoch==1 or nowEpoch<epoch:
+        print ("epoch: ", nowEpoch)
         for i in range(0, 400):
             for j in range(0, 300):
-                a = w[0] * picK1.getpixel((i, j)) +
-                    w[1] * picK2.getpixel((i, j)) +
+                a = w[0] * picK1.getpixel((i, j)) + \
+                    w[1] * picK2.getpixel((i, j)) + \
                     w[2] * picI.getpixel((i, j))
                 error = (picE.getpixel((i, j)) - a)
+                w[0] += rate * error * picK1.getpixel((i, j))
+                w[1] += rate * error * picK2.getpixel((i, j))
+                w[2] += rate * error * picE.getpixel((i, j))
+
+        nowEpoch += 1
+        print ("error: ", error)
+
+    return w
+
+print (mseGD())
